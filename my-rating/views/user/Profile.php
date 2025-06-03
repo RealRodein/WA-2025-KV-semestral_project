b83ecf396f4bddbf5317203c934879cb6f5570ba
Vojtěch Kupec <rodein.personal@gmail.com>
2025-06-03 21:43:17 +0200

    #4

---
+++ my-rating/views/user/Profile.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title>Svět Seriálů</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/WA-2025-KV-semestral_project/my-rating/public/css/style.css">
+</head>
+<body>
+    <div class="header">
+        <a class="logo" href="List.php">
+            <img src="/WA-2025-KV-semestral_project/my-rating/public/images/icon.svg" alt="Moje-Hodnocení" style="width:32px;height:32px;vertical-align:middle;">
+        </a>
+
+        <div class="menu">
+            <a href="../auth/Login.php">Procházet</a>
+            <a href="../auth/Login.php">Profil</a>
+            <a href="../auth/Login.php">Recenze</a>
+        </div>
+        
+        <div class="user">
+            <a class="user" href="../auth/Login.php">Přihlášení</a>
+            <a class="user" href="../auth/Register.php">Registrace</a>
+        </div>
+    </div>
+
+    <div class="main-wrapper">
+        <div class="sidebar"></div>
+
+        <div class="content">
+            <!-- Your main page content goes here -->
+        </div>
+
+        <div class="sidebar"></div>
+    </div>
+</body>
+</html>
