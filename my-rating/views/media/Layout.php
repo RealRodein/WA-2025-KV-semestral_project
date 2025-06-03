c0f40c8847f98dac9b15850c14a00f06e5ccff23
Vojtěch Kupec <rodein.personal@gmail.com>
2025-06-03 21:40:46 +0200

    #1 hotovo

---
+++ my-rating/views/media/Layout.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title>Moje-Hodnocení</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/WA-2025-KV-semestral_project/my-rating/public/css/style.css">
+</head>
+<body>
+    <div class="header">
+        <a class="logo" href="/WA-2025-KV-semestral_project/my-rating/controllers/MediaController.php">
+            <img src="/WA-2025-KV-semestral_project/my-rating/public/images/icon.svg" alt="Moje-Hodnocení" style="width:32px;height:32px;vertical-align:middle;">
+        </a>
+
+        <div class="menu">
+            <a href="/WA-2025-KV-semestral_project/my-rating/controllers/MediaController.php">Procházet</a>
+            <a href="/WA-2025-KV-semestral_project/my-rating/views/user/Profile.php">Profil</a>
+        </div>
+        
+        <div class="user">
+            <a class="user" href="/WA-2025-KV-semestral_project/my-rating/views/auth/Login.php">Přihlášení</a>
+            <a class="user" href="/WA-2025-KV-semestral_project/my-rating/views/auth/Register.php">Registrace</a>
+        </div>
+    </div>
+
+    <div class="main-wrapper">
+        <div class="sidebar"></div>
+
+        <div class="content">
+            <!-- Your main page content goes here -->
+        </div>
+
+        <div class="sidebar"></div>
+    </div>
+</body>
+</html>
